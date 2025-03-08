1ef7a515cd55585e32e673bcee893df5d3ee66f4
ISKinGeR <51255680+ISKinGeR@users.noreply.github.com>
2025-03-08 17:46:03 +0300

    Add files via upload

---
+++ sprays/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mercy Collective'
+description 'Spray Assets'
+version '1.0.0'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ytyp/mercy_sprays.ytyp'
+
+-- https://dsc.gg/mercy-coll/ - Mercy Collective
